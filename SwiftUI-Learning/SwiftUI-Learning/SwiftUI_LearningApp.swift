b6c530ee6c6ff303ae18561d76e5af8fdcd74c93
Eduardo R Vieira <eduardovieirayt@gmail.com>
2024-09-02 06:32:04 -0300

    create project

---
+++ SwiftUI-Learning/SwiftUI-Learning/SwiftUI_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LearningApp.swift
+//  SwiftUI-Learning
+//
+//  Created by Eduardo on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
